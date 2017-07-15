457a33af410d876e39f6a2d929227855d0fb88e6
tetradog <tetradog@gmail.com>
2017-07-15 15:55:55 +0200

    solucionando unos problemillas

--- SistemaMTBW/Imagenes.Designer.cs
+++ SistemaMTBW/Imagenes.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MOSinMedallas {
+namespace BorrarMos {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MOSinMedallas {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MOSinMedallas.Imagenes", typeof(Imagenes).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BorrarMos.Imagenes", typeof(Imagenes).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- SistemaMTBW/MainWindow.xaml.cs
+++ SistemaMTBW/MainWindow.xaml.cs
@@ -20,88 +20,81 @@ using System.Windows.Shapes;
 
 namespace MOSinMedallas
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        RomGba rom;
-        EdicionPokemon edicion;
-        Compilacion compilacion;
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		RomGba rom;
+		EdicionPokemon edicion;
+		Compilacion compilacion;
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Esta aplicación permite inGAME poder borrar una  MO como un ataque más \n\nDesarrollado por Pikachu240 investigado por JPAN", "Sobre la App");
-        }
+		private void MenuItem_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Esta aplicación permite inGAME poder borrar una  MO como un ataque más \n\nDesarrollado por Pikachu240 investigado por JPAN", "Sobre la App");
+		}
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog opnRom = new OpenFileDialog();
-            opnRom.Filter = "Rom Pokemon GBA|*.gba";
-            try
-            {
-                if (opnRom.ShowDialog().GetValueOrDefault())
-                {
-                    rom = new RomGba(opnRom.FileName);
-                    edicion=EdicionPokemon.GetEdicionPokemon(rom);
-                    compilacion=Compilacion.GetCompilacion(rom,edicion);
-                    PonTexto();
-                    btnPonerOQuitar.IsEnabled = true;
-                    switch(edicion.AbreviacionRom)
-                    {
-                        case AbreviacionCanon.BPE:imgDecoración.SetImage(Imagenes.PokeballEsmeralda);break;
-                        case AbreviacionCanon.BPR: imgDecoración.SetImage(Imagenes.PokeballRojoFuego); break;
-                        case AbreviacionCanon.BPG: imgDecoración.SetImage(Imagenes.PokeballVerdeHoja); break;
-                        case AbreviacionCanon.AXV: imgDecoración.SetImage(Imagenes.PokeballRuby); break;
-                        case AbreviacionCanon.AXP: imgDecoración.SetImage(Imagenes.PokeballZafiro); break;
-                    }
-                }
-                else if(rom!=null)
-                {
-                    MessageBox.Show("No se ha cambiado la rom","Atención",MessageBoxButton.OK,MessageBoxImage.Exclamation);
-                }else
-                {
-                    MessageBox.Show("No se ha cargado nada...");
-                }
-            }catch
-            {
-                btnPonerOQuitar.IsEnabled = false;
-                rom = null;
-                imgDecoración.SetImage(new Bitmap(1, 1));
-                MessageBox.Show("La rom no es compatible","Aun no es universal...");
-            }
-        }
+		private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog opnRom = new OpenFileDialog();
+			opnRom.Filter = "Rom Pokemon GBA|*.gba";
+			
+			if (opnRom.ShowDialog().GetValueOrDefault())
+			{
+				rom = new RomGba(opnRom.FileName);
+				edicion=EdicionPokemon.GetEdicionPokemon(rom);
+				compilacion=Compilacion.GetCompilacion(rom,edicion);
+				PonTexto();
+				btnPonerOQuitar.IsEnabled = true;
+				switch(edicion.AbreviacionRom)
+				{
+						case AbreviacionCanon.BPE:imgDecoración.SetImage(BorrarMos.Imagenes.PokeballEsmeralda);break;
+						case AbreviacionCanon.BPR: imgDecoración.SetImage(BorrarMos.Imagenes.PokeballRojoFuego); break;
+						case AbreviacionCanon.BPG: imgDecoración.SetImage(BorrarMos.Imagenes.PokeballVerdeHoja); break;
+						case AbreviacionCanon.AXV: imgDecoración.SetImage(BorrarMos.Imagenes.PokeballRuby); break;
+						case AbreviacionCanon.AXP: imgDecoración.SetImage(BorrarMos.Imagenes.PokeballZafiro); break;
+				}
+			}
+			else if(rom!=null)
+			{
+				MessageBox.Show("No se ha cambiado la rom","Atención",MessageBoxButton.OK,MessageBoxImage.Exclamation);
+			}else
+			{
+				MessageBox.Show("No se ha cargado nada...");
+			}
+			
+		}
 
-        private void PonTexto()
-        {
-        	if(PokemonGBAFrameWork.BorrarMos.EstaActivado(rom,edicion,compilacion))
-            {
-                btnPonerOQuitar.Content = "Volver MO con medallas";
-            }
-           else
-            {
-                btnPonerOQuitar.Content = "Poner MO sin MEDALLAS!";
-            }
-        }
+		private void PonTexto()
+		{
+			if(PokemonGBAFrameWork.BorrarMos.EstaActivado(rom,edicion,compilacion))
+			{
+				btnPonerOQuitar.Content = "MOs IMBORRABLES";
+			}
+			else
+			{
+				btnPonerOQuitar.Content = "MOs Olvidables";
+			}
+		}
 
-        private void btnPonerOQuitar_Click(object sender, RoutedEventArgs e)
-        {
-            if (PokemonGBAFrameWork.BorrarMos.EstaActivado(rom,edicion,compilacion))
-            {
-                PokemonGBAFrameWork.BorrarMos.Desactivar(rom, edicion,compilacion);
-             
-            }
-            else
-            {
-                PokemonGBAFrameWork.BorrarMos.Activar(rom, edicion,compilacion);
-   
-            }
-            PonTexto();
-            rom.Save();
-        }
-    }
+		private void btnPonerOQuitar_Click(object sender, RoutedEventArgs e)
+		{
+			if (PokemonGBAFrameWork.BorrarMos.EstaActivado(rom,edicion,compilacion))
+			{
+				PokemonGBAFrameWork.BorrarMos.Desactivar(rom, edicion,compilacion);
+				
+			}
+			else
+			{
+				PokemonGBAFrameWork.BorrarMos.Activar(rom, edicion,compilacion);
+				
+			}
+			PonTexto();
+			rom.Save();
+		}
+	}
 }
